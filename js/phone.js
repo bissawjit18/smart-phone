8e8192ee2e1194e372dd4db6e61258cd43ea1c06
bissawjit <bissawjit18@gmail.com>
2024-03-10 10:29:51 +0600

    first commit

--- js/phone.js
+++ js/phone.js
@@ -1,3 +1,21 @@
+ /**  # phone-hunter-api
+
+## API Links
+
+### Phone Search
+URL Format: https://openapi.programming-hero.com/api/phones?search=${searchText}
+
+Example: https://openapi.programming-hero.com/api/phones?search=iphone
+
+
+### Phone detail url:
+URL Format: https://openapi.programming-hero.com/api/phone/${id}
+
+
+Example: https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089
+
+ */
+ 
 const loadPhone = async (searchText='samsung', isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
